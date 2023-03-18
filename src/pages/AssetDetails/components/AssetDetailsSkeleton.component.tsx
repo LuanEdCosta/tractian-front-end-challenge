59ea3d8ef12849006a9e28e525fe731aa6944822
Luan Eduardo <luan1346@gmail.com>
2023-03-18 15:29:51 -0300

    fix: asset details page skeleton

--- src/pages/AssetDetails/components/AssetDetailsSkeleton.component.tsx
+++ src/pages/AssetDetails/components/AssetDetailsSkeleton.component.tsx
@@ -1,7 +1,25 @@
-import { Skeleton } from '@mui/material'
+import { Grid, Skeleton, Stack } from '@mui/material'
 
 export function AssetDetailsSkeleton() {
   return (
-    <Skeleton sx={{ borderRadius: 2 }} variant="rectangular" height={400} />
+    <Stack>
+      <Grid mx="-1rem !important" spacing={2} wrap="wrap-reverse" container>
+        <Grid xs={12} sm={6} md={7} lg={8} item>
+          <Skeleton
+            sx={{ borderRadius: 2 }}
+            variant="rectangular"
+            height={300}
+          />
+        </Grid>
+
+        <Grid xs={12} sm={6} md={5} lg={4} item>
+          <Skeleton
+            sx={{ borderRadius: 2 }}
+            variant="rectangular"
+            height={600}
+          />
+        </Grid>
+      </Grid>
+    </Stack>
   )
 }

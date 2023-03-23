e7ee1dd5f6ec5094f7ea666c535f0a70c1ea6411
Luan Eduardo <luan1346@gmail.com>
2023-03-23 12:41:57 -0300

    fix: form skeleton components

--- src/pages/CreateWorkOrder/components/WorkOrderFormSkeleton.component.tsx
+++ src/pages/CreateWorkOrder/components/WorkOrderFormSkeleton.component.tsx
@@ -1,27 +1,34 @@
-import { Grid, Skeleton } from '@mui/material'
+import { Grid, Skeleton, Stack } from '@mui/material'
 
 export function WorkOrderFormSkeleton() {
   return (
-    <Grid mx="-1rem !important" justifyContent="flex-end" spacing={2} container>
-      {new Array(1).fill(0).map((_, index) => {
-        return (
-          <Grid key={index} xs={12} item>
-            <Skeleton
-              sx={{ borderRadius: 1 }}
-              variant="rectangular"
-              height="3.6rem"
-            />
-          </Grid>
-        )
-      })}
+    <Stack>
+      <Grid
+        mx="-2rem !important"
+        justifyContent="flex-end"
+        spacing={4}
+        container
+      >
+        {new Array(4).fill(0).map((_, index) => {
+          return (
+            <Grid key={index} xs={12} sm={6} item>
+              <Skeleton
+                sx={{ borderRadius: 1 }}
+                variant="rectangular"
+                height="3.6rem"
+              />
+            </Grid>
+          )
+        })}
 
-      <Grid xs={12} sm={6} md={4} lg={3} item>
-        <Skeleton
-          sx={{ borderRadius: 1 }}
-          variant="rectangular"
-          height="3.6rem"
-        />
+        <Grid xs={12} sm={6} md={4} lg={3} item>
+          <Skeleton
+            sx={{ borderRadius: 1 }}
+            variant="rectangular"
+            height="2.8rem"
+          />
+        </Grid>
       </Grid>
-    </Grid>
+    </Stack>
   )
 }
